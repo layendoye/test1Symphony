e9e251c64e04175e0350b37404deec9900878500
Abdoulaye Ndoye <layendoyesn@gmail.com>
2019-06-28 19:04:52 +0000

    vs

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,16 +4,28 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function index()
+    public function index(ArticleRepository $repo)
     {
+        //$repo=$this->getDoctrine()->getRepository(Article::class);//demander à doctrine de nous creer un repository pour gerer nos articles (et pouvoir selectionner des données)
+        // remplacer par ArticleRepository $repo
+
+
+        //$article=$repo->find(12);//renvoi l'article avec l'id 12
+        //$article=$repo->findOneByTitle('Titre de l\'article');//un article qui a ce titre
+        //$articles=$repo->findByTitle('Titre de l\'article');//tous les articles qui ont ce titre
+        $articles=$repo->findAll();//tous les articles
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles'=>$articles
         ]);
     }
     /**
@@ -22,14 +34,16 @@ class BlogController extends AbstractController
     public function home(){
         return $this->render('blog/home.html.twig',[
             'title'=>"Bienvenue mes amis",
-            "age"=>20,
+            "age"=>20
         ]);
     }
 
     /**
-     * @Route ("/blog/12", name="blog_show")
+     * @Route ("/blog/{id}", name="blog_show")
      */
-    public function show(){
-        return $this->render("blog/show.html.twig");
+    public function show(Article $article){//donnera l'article avec l'id données dans le path('blog_show',{'id': article.id })
+        return $this->render("blog/show.html.twig",[
+            'article'=>$article
+        ]);
     }
 }
