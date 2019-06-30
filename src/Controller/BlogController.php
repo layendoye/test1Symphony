d42cde1c5a9cce4e50df1b96751e73ce149f533e
Abdoulaye Ndoye <layendoyesn@gmail.com>
2019-06-30 15:23:46 +0000

    Nouvelle Version

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\ArticleType;
 
 class BlogController extends AbstractController
 {//la classe est crée lorsqu'on a creer notre controller en faisant php bin/console make:controller
@@ -37,6 +40,33 @@ class BlogController extends AbstractController
     }
 
     /**
+     *@Route("/blog/new", name="blog_create")
+     *@Route("/blog/{id}/edit", name="blog_edit")
+    */
+    public function form(Article $article=null,Request $request, ObjectManager $manager){//$request contiendra les données du formulaire
+        if(!$article){
+           $article=new Article(); //si je n'est pas d article on cree un article vide car si on modifie il y aura un article (celui donné par Article $article)
+        }
+        $form=$this->createForm(ArticleType::class,$article);//on appel le formulaire créé via la console et on le lie avec $article
+
+        $form->handleRequest($request);//analyse de la requette (recu apres envoi du formulaire)
+        
+        if($form->isSubmitted() && $form->isValid()){
+            if(!$article->getId()){//si l article n a pas d Id ca veut dire que c est un nouveau on fait ca pour ne pas modifier la date de création lorsqu on modifie un article
+               $article->setCreatedAt(new \DateTime()); 
+            }
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show',['id'=>$article->getId()]);
+        }
+        return $this->render('blog/create.html.twig',[
+            'formArticle'=>$form->createView(),
+            'editMode' => ($article->getId() !==null) //si lorsqu on recharge la page pour une premier fois l'id existe editMod=true 
+        ]);
+    }
+
+    /**
      * @Route ("/blog/{id}", name="blog_show")
      */
     public function show(Article $article){//donnera l'article avec l'id données dans le path('blog_show',{'id': article.id })
@@ -45,4 +75,6 @@ class BlogController extends AbstractController
             'article'=>$article
         ]);
     }
+
+    
 }
--- src/DataFixtures/ArticlesFixtures.php
+++ src/DataFixtures/ArticlesFixtures.php
@@ -5,20 +5,44 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
 
 class ArticlesFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for($i=1;$i<=10;$i++){
-            $article=new Article();
-            $article->setTitle('Titre de l\'article n'.$i.'')
-                    ->setContent("<p>Contenu de l'article numero $i</p>")
-                    ->setImage("http://placehold.it/350x150")
-                    ->setCreatedAt(new \DateTime());
-            $manager->persist($article);//prepare les articles
+        $faker = \Faker\Factory::create('fr_FR');
+        for($i=1;$i<=3;$i++){
+            $category=new Category();
+            $category->setTitle($faker->sentence())
+                     ->setDescription($faker->paragraph());
+            $manager->persist($category);
+            for($j=1;$j<=mt_rand(4,6);$j++){
+                $article=new Article();
+                
+                $article->setTitle($faker->sentence())
+                        ->setContent('<p>'.join($faker->paragraphs(5),'</p><p>').'</p>')
+                        ->setImage($faker->imageUrl())
+                        ->setCreatedAt($faker->dateTimeBetween('-6 months'))
+                        ->setCategory($category);
+                $manager->persist($article);//prepare les articles
+                
+                $now=new \DateTime();
+                $interval=$now->diff($article->getCreatedAt());
+                $days=$interval->days;
+                $minimum='-'.$days.' days';//exemple -100 days
+                
+                for($k=1;$k<=mt_rand(4,10);$k++){
+                    $comment=new Comment();
+                    $comment->setAuthor($faker->name)
+                            ->setContent('<p>'.join($faker->paragraphs(2),'</p><p>').'</p>')
+                            ->setCreatedAt($faker->dateTimeBetween($minimum))
+                            ->setArticle($article);
+                    $manager->persist($comment);
+                }
+            }
         }
-
         $manager->flush();//execute la préparation
     }
 }
